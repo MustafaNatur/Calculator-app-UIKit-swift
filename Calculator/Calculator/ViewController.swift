83195a53a76a26a5483ec187681e104a0ca9e3a5
Natur Mustafa <mustafa.natur@mail.ru>
2022-10-07 15:39:22 +0300

    develop:start

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Мустафа Натур on 07.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
